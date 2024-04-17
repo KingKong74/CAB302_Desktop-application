1b69c2e600142b7eef31cc53a37c03321016e000
Bailey <Bailey.king37@gmail.com>
2024-04-17 22:17:55 +1000

    Framework setup

--- src/main/java/com/example/main_sem_proj/HelloApplication.java
+++ src/main/java/com/example/main_sem_proj/HelloApplication.java
@@ -4,15 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.stage.Screen;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    // Constants for width and heigh
+    private static final double WIDTH = 700;
+    private static final double HEIGHT = 330;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("HEYA!");
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        stage.setTitle("Iz.Lumin");
+
+        // Set the application icon
+        // stage.getIcons().add(new Image("path for image"));
+
+        // Calculate the X and Y coordinates for bottom right corner
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+        double bottomRightX = screenWidth - (WIDTH + 9);
+        double bottomRightY = screenHeight - (HEIGHT + 70);
+
+        // Set the position of the stage
+        stage.setX(bottomRightX);
+        stage.setY(bottomRightY);
+
+        stage.setResizable(false); // changed to cannot resize (BK)
+
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/main_sem_proj/model/SqliteConnection.java
@@ -0,0 +1,4 @@
+package com.example.main_sem_proj.model;
+
+public class SqliteConnection {
+}
