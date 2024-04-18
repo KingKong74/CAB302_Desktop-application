3e102d76eaa445830954a580634d6ee4da275e83
Bailey <Bailey.king37@gmail.com>
2024-04-18 20:57:43 +1000

    Fixed up Hello/Main controller confusion and started on MAIN GUI build. No functions

--- src/main/java/com/example/main_sem_proj/controller/HelloController.java
+++ src/main/java/com/example/main_sem_proj/controller/HelloController.java
@@ -1,14 +1,48 @@
 package com.example.main_sem_proj.controller;
 
+import com.example.main_sem_proj.HelloApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    public Button loginButton;
+    private final int WIDTH = 700;
+    private final int HEIGHT = 350;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick() throws IOException {
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        stage.setScene(scene);
+
+        // Calculate the X and Y coordinates for bottom right corner
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+        double bottomRightX = screenWidth - (WIDTH + 9);
+        double bottomRightY = screenHeight - (HEIGHT + 70);
+
+        // Set the position of the stage
+        stage.setX(bottomRightX);
+        stage.setY(bottomRightY);
+
+        stage.show();
+
+
     }
 }
+
+// (BK)....
+// Set the application icon
+// stage.getIcons().add(new Image("path for image"));
+
+
