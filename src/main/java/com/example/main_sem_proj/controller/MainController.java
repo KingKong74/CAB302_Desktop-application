9ebf3619c8e13c536710520424023f6004cfc2d5
Bailey <Bailey.king37@gmail.com>
2024-04-20 18:27:14 +1000

    Started on mainview app functionality, simple tests for main-GUI also

--- src/main/java/com/example/main_sem_proj/controller/MainController.java
+++ src/main/java/com/example/main_sem_proj/controller/MainController.java
@@ -1,26 +1,92 @@
 package com.example.main_sem_proj.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+
 
 public class MainController {
+    public Setting getCurrentSetting() {
+        return currentSetting;
+    }
+
+    public enum Setting {
+        DARK_MODE,
+        FOCUS_MODE,
+
+        // TODO: add more settings later...
+    }
+
+    private Setting currentSetting = null;
 
     @FXML
     protected void onDarkModeButtonClick() {
-//        welcomeText.setText("Works");
+        handleSwitchClick(Setting.DARK_MODE);
     }
+
     @FXML
     protected void onFocusModeButtonClick() {
-//        welcomeText.setText("Works");
+        handleSwitchClick(Setting.FOCUS_MODE);
     }
+
+    //
+    //TODO add others...
+    //
+
+    @FXML
+    public void handleSwitchClick(Setting setting) {
+        if (currentSetting != setting) {
+            currentSetting = setting;
+            handleSettingChange(setting);
+        }else {
+            System.out.println( currentSetting + " disabled");
+            currentSetting = null;
+        }
+    }
+
+    private void handleSettingChange(Setting setting) {
+        switch (setting) {
+            case DARK_MODE:
+                System.out.println("Dark Mode enabled");
+                break;
+            case FOCUS_MODE:
+                System.out.println("Focus Mode enabled");
+                break;
+            // TODO more settings for the other presets
+        }
+    }
+
+    enum Popup {
+        NOTIFICATIONS,
+        SETTINGS
+    }
+
+    private Popup currentPopup = null;
+
     @FXML
     protected void onNotificationsButtonClick() {
-//        welcomeText.setText("Works");
+        handlePopup(Popup.NOTIFICATIONS);
     }
     @FXML
     protected void onSettingsButtonClick() {
-//        welcomeText.setText("Works");
+        handlePopup(Popup.SETTINGS);
+    }
+
+    private void handlePopup(Popup popup) {
+        if (currentPopup != popup) {
+            closePopup();
+            openPopup(popup);
+        }
+    }
+
+    private void openPopup(Popup popup) {
+        System.out.println("Opening " + popup.toString() + " popup");
+        currentPopup = popup;
+    }
+
+    private void closePopup() {
+        if (currentPopup != null) {
+            System.out.println("Closing " + currentPopup.toString() + " popup");
+            currentPopup = null;
+        }
     }
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,5 @@ module com.example.main_sem_proj {
     opens com.example.main_sem_proj to javafx.fxml;
     opens com.example.main_sem_proj.controller to javafx.fxml;
     exports com.example.main_sem_proj;
+    exports com.example.main_sem_proj.controller;
 }
