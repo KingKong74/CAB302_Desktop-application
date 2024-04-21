0557ef8b332869aad29f3ea475cde325f167a4d5
Charlie <n11595710@qut.edu.au>
2024-04-21 21:13:35 +1000

    added tests and validation method

---
+++ src/main/java/com/example/main_sem_proj/model/Validation.java
@@ -0,0 +1,10 @@
+package com.example.main_sem_proj.model;
+
+public class Validation {
+
+    public Boolean ValidateEmail(String Email){
+
+        return null;
+}
+
+}
---
+++ src/test/java/ValidateEmailTest.java
@@ -0,0 +1,55 @@
+import com.example.main_sem_proj.model.Validation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ValidateEmailTest {
+// the function should right now, return true if the email is valid and false if it is not
+ private Validation validation;
+    @BeforeEach
+    void setup() { validation = new Validation();}
+
+
+    @Test
+    void validateEmail_withValidEmail(){
+    String Test = "n11595710@qut.edu.au";
+    Boolean result = validation.ValidateEmail(Test);
+    assertEquals(true,result);
+}
+    @Test
+    void validateEmail_withNullEmail(){
+        String Test = "";
+        Boolean result = validation.ValidateEmail(Test);
+        assertEquals(false,result);
+    }
+    @Test
+    void validateEmail_withNoAtSymbolButDotCom(){
+        String Test = "Charliekerr2004.com";
+        Boolean result = validation.ValidateEmail(Test);
+        assertEquals(false,result);
+    }
+    @Test
+    void validateEmail_withAtSymbolButNoDotCom(){
+        String Test = "Trainrunner47.com";
+        Boolean result = validation.ValidateEmail(Test);
+        assertEquals(false, result);
+    }
+    @Test
+    void validateEmail_withNoAtSymbolAndNoDotCom(){
+        String Test = "KingKong";
+        Boolean result = validation.ValidateEmail(Test);
+        assertEquals(false, result);
+    }
+    @Test
+    void validateEmail_withInvalidCharacterAfterAt(){
+        String Test = "Charliekerr@gm#il.com";
+        Boolean result = validation.ValidateEmail(Test);
+        assertEquals(false, result);
+    }
+
+
+
+
+
+}
