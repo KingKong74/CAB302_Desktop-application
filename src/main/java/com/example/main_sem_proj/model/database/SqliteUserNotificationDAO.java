8bb19c638f11131d81d3c288eb9527b01ae2d893
Ty <tythie1972@gmail.com>
2024-05-25 14:55:48 +1000

    Started work on notification functionality

--- src/main/java/com/example/main_sem_proj/model/database/SqliteUserNotificationDAO.java
+++ src/main/java/com/example/main_sem_proj/model/database/SqliteUserNotificationDAO.java
@@ -1,6 +1,11 @@
 package com.example.main_sem_proj.model.database;
 
+import com.example.main_sem_proj.controller.NotificationsController;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SqliteUserNotificationDAO {
 
@@ -16,4 +21,29 @@ public class SqliteUserNotificationDAO {
                 + ")";
         sqlite.createTable(query);
     }
+
+    public void insert(NotificationSetting notificationSetting) {
+
+    }
+
+    public NotificationSetting select(String title) {
+        String sql = "SELECT * FROM notificationSettings WHERE title = ?";
+        NotificationSetting notificationSetting = null;
+        try {
+            PreparedStatement insertDetails = connection.prepareStatement(sql);
+            insertDetails.setString(1, title);
+            ResultSet rs = insertDetails.executeQuery();
+
+            if (rs.next()) {
+                notificationSetting = new NotificationSetting(
+                        rs.getString("title"),
+                        rs.getString("Description"));
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return notificationSetting;
+    }
+
 }
